7fdd982b045dbdb1624a8eb1dead27c2d49c88ff
mateusgaudencio14 <mateusdvlp@gmail.com>
2022-03-24 10:34:44 -0300

    creat template base

--- Modules/Backoffice/Resources/views/layouts/master.blade.php
+++ Modules/Backoffice/Resources/views/layouts/master.blade.php
@@ -1,19 +1,261 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Module Backoffice</title>
+@extends('base::layouts.lte')
+@section('menu')
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+        <!-- Left navbar links -->
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            </li>
+            <li class="nav-item d-none d-sm-inline-block">
+                <a href="../../index3.html" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item d-none d-sm-inline-block">
+                <a href="#" class="nav-link">Contact</a>
+            </li>
+        </ul>
 
-       {{-- Laravel Mix - CSS File --}}
-       {{-- <link rel="stylesheet" href="{{ mix('css/backoffice.css') }}"> --}}
+        <!-- Right navbar links -->
+        <ul class="navbar-nav ml-auto">
+            <!-- Navbar Search -->
+            <li class="nav-item">
+                <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+                    <i class="fas fa-search"></i>
+                </a>
+                <div class="navbar-search-block">
+                    <form class="form-inline">
+                        <div class="input-group input-group-sm">
+                            <input class="form-control form-control-navbar" type="search" placeholder="Search"
+                                   aria-label="Search">
+                            <div class="input-group-append">
+                                <button class="btn btn-navbar" type="submit">
+                                    <i class="fas fa-search"></i>
+                                </button>
+                                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </li>
 
-    </head>
-    <body>
-        @yield('content')
+            <!-- Messages Dropdown Menu -->
+            <li class="nav-item dropdown">
+                <a class="nav-link" data-toggle="dropdown" href="#">
+                    <i class="far fa-comments"></i>
+                    <span class="badge badge-danger navbar-badge">3</span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                    <a href="#" class="dropdown-item">
+                        <!-- Message Start -->
+                        <div class="media">
+                            <img src="../../dist/img/user1-128x128.jpg" alt="User Avatar"
+                                 class="img-size-50 mr-3 img-circle">
+                            <div class="media-body">
+                                <h3 class="dropdown-item-title">
+                                    Brad Diesel
+                                    <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                                </h3>
+                                <p class="text-sm">Call me whenever you can...</p>
+                                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                            </div>
+                        </div>
+                        <!-- Message End -->
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a href="#" class="dropdown-item">
+                        <!-- Message Start -->
+                        <div class="media">
+                            <img src="../../dist/img/user8-128x128.jpg" alt="User Avatar"
+                                 class="img-size-50 img-circle mr-3">
+                            <div class="media-body">
+                                <h3 class="dropdown-item-title">
+                                    John Pierce
+                                    <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
+                                </h3>
+                                <p class="text-sm">I got your message bro</p>
+                                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                            </div>
+                        </div>
+                        <!-- Message End -->
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a href="#" class="dropdown-item">
+                        <!-- Message Start -->
+                        <div class="media">
+                            <img src="../../dist/img/user3-128x128.jpg" alt="User Avatar"
+                                 class="img-size-50 img-circle mr-3">
+                            <div class="media-body">
+                                <h3 class="dropdown-item-title">
+                                    Nora Silvester
+                                    <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
+                                </h3>
+                                <p class="text-sm">The subject goes here</p>
+                                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                            </div>
+                        </div>
+                        <!-- Message End -->
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+                </div>
+            </li>
+            <!-- Notifications Dropdown Menu -->
+            <li class="nav-item dropdown">
+                <a class="nav-link" data-toggle="dropdown" href="#">
+                    <i class="far fa-bell"></i>
+                    <span class="badge badge-warning navbar-badge">15</span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                    <span class="dropdown-item dropdown-header">15 Notifications</span>
+                    <div class="dropdown-divider"></div>
+                    <a href="#" class="dropdown-item">
+                        <i class="fas fa-envelope mr-2"></i> 4 new messages
+                        <span class="float-right text-muted text-sm">3 mins</span>
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a href="#" class="dropdown-item">
+                        <i class="fas fa-users mr-2"></i> 8 friend requests
+                        <span class="float-right text-muted text-sm">12 hours</span>
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a href="#" class="dropdown-item">
+                        <i class="fas fa-file mr-2"></i> 3 new reports
+                        <span class="float-right text-muted text-sm">2 days</span>
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                    <i class="fas fa-expand-arrows-alt"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+                    <i class="fas fa-th-large"></i>
+                </a>
+            </li>
+        </ul>
+    </nav>
 
-        {{-- Laravel Mix - JS File --}}
-        {{-- <script src="{{ mix('js/backoffice.js') }}"></script> --}}
-    </body>
-</html>
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <!-- Brand Logo -->
+    {{--<a href="../../index3.html" class="brand-link">--}}
+    {{--<img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"--}}
+    {{--style="opacity: .8">--}}
+    {{--<span class="brand-text font-weight-light">AdminLTE 3</span>--}}
+    {{--</a>--}}
+
+    <!-- Sidebar -->
+        <div class="sidebar">
+            <!-- Sidebar user (optional) -->
+            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                <div class="image">
+                    <img src="#" class="img img-fluid" alt="Admin vem money" style="width: 120px;">
+                </div>
+
+            </div>
+
+            <!-- SidebarSearch Form -->
+            <div class="form-inline">
+                {{--<div class="input-group" data-widget="sidebar-search">--}}
+                {{--<input class="form-control form-control-sidebar" type="search" placeholder="Search"--}}
+                {{--aria-label="Search">--}}
+                {{--<div class="input-group-append">--}}
+                {{--<button class="btn btn-sidebar">--}}
+                {{--<i class="fas fa-search fa-fw"></i>--}}
+                {{--</button>--}}
+                {{--</div>--}}
+                {{--</div>--}}
+                <div class="sidebar-search-results">
+                    <div class="list-group"><a href="#" class="list-group-item">
+                            <div class="search-title"><strong class="text-light"></strong>N<strong
+                                    class="text-light"></strong>o<strong class="text-light"></strong> <strong
+                                    class="text-light"></strong>e<strong class="text-light"></strong>l<strong
+                                    class="text-light"></strong>e<strong class="text-light"></strong>m<strong
+                                    class="text-light"></strong>e<strong class="text-light"></strong>n<strong
+                                    class="text-light"></strong>t<strong class="text-light"></strong> <strong
+                                    class="text-light"></strong>f<strong class="text-light"></strong>o<strong
+                                    class="text-light"></strong>u<strong class="text-light"></strong>n<strong
+                                    class="text-light"></strong>d<strong class="text-light"></strong>!<strong
+                                    class="text-light"></strong></div>
+                            <div class="search-path"></div>
+                        </a></div>
+                </div>
+            </div>
+
+            <!-- Sidebar Menu -->
+            <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                    data-accordion="false">
+                    <!-- Add icons to the links using the .nav-icon class
+                         with font-awesome or any other icon font library -->
+                    {{--<li class="nav-item">--}}
+                        {{--<a href="{{route('admin.dashboard-index')}}" class="nav-link">--}}
+                            {{--<i class="fa fa-tachometer-alt"></i>--}}
+                            {{--<p>--}}
+                                {{--Dashboard--}}
+                            {{--</p>--}}
+                        {{--</a>--}}
+                    {{--</li>--}}
+                    {{--<li class="nav-item">--}}
+                        {{--<a href="{{route('admin.lead-index')}}" class="nav-link">--}}
+                            {{--<i class="fa fa-user"></i>--}}
+                            {{--<p>--}}
+                                {{--Leads--}}
+                            {{--</p>--}}
+                        {{--</a>--}}
+                    {{--</li>--}}
+                    {{--<li class="nav-item">--}}
+                        {{--<a href="{{route('admin.especialidade-index')}}" class="nav-link">--}}
+                            {{--<i class="fa fa-list"></i>--}}
+                            {{--<p>--}}
+                                {{--Especialidades--}}
+                            {{--</p>--}}
+                        {{--</a>--}}
+                    {{--</li>--}}
+                    {{--<li class="nav-item">--}}
+                        {{--<a href="{{route('admin.finalidade-index')}}" class="nav-link">--}}
+                            {{--<i class="fa fa-list"></i>--}}
+                            {{--<p>--}}
+                                {{--Finalidades--}}
+                            {{--</p>--}}
+                        {{--</a>--}}
+                    {{--</li>--}}
+                    {{--<li class="nav-item">--}}
+                        {{--<a href="{{route('admin.parceiro-index')}}" class="nav-link">--}}
+                            {{--<i class="fa fa-handshake"></i>--}}
+                            {{--<p>--}}
+                                {{--Parceiros--}}
+                            {{--</p>--}}
+                        {{--</a>--}}
+                    {{--</li>--}}
+                    {{--<li class="nav-item">--}}
+                        {{--<a href="{{route('admin.admin-change-password')}}" class="nav-link">--}}
+                            {{--<i class="fa fa-lock"></i>--}}
+                            {{--<p>Alterar senha--}}
+                            {{--</p>--}}
+                        {{--</a>--}}
+                    {{--</li>--}}
+                    {{--<li class="nav-item">--}}
+                        {{--<a href="{{ route('admin.logout') }}"--}}
+                           {{--onclick="event.preventDefault();--}}
+                                {{--document.getElementById('logout-form-admin').submit();" class="nav-link">--}}
+                            {{--<i class="fa fa-power-off"></i>--}}
+                            {{--<span>Logout</span>--}}
+                        {{--</a>--}}
+                        {{--<form id="logout-form-admin" action="{{ route('admin.logout') }}" method="POST"--}}
+                              {{--style="display: none;">--}}
+                            {{--@csrf--}}
+                        {{--</form>--}}
+                    {{--</li>--}}
+                </ul>
+            </nav>
+            <!-- /.sidebar-menu -->
+        </div>
+        <!-- /.sidebar -->
+    </aside>
+@endsection
+
---
+++ Modules/Base/Resources/views/layouts/lte.blade.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Clips Web</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{asset('vendor/lte/plugins/fontawesome-free/css/all.min.css')}}">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet"
+          href="{{asset('vendor/lte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{asset('vendor/lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="{{asset('vendor/lte/plugins/jqvmap/jqvmap.min.css')}}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('vendor/lte/dist/css/adminlte.min.css')}}">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="{{asset('vendor/lte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="{{asset('vendor/lte/plugins/daterangepicker/daterangepicker.css')}}">
+    <!-- summernote -->
+    <link rel="stylesheet" href="{{asset('vendor/lte/plugins/summernote/summernote-bs4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('vendor/lte/plugins/toastr/toastr.min.css')}}">
+
+    <script src="{{asset('vendor/lte/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
+
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+    <!-- Preloader -->
+    <div class="preloader flex-column justify-content-center align-items-center">
+        <img class="animation__shake" src="#" alt="Vem money" height="200" width="200">
+    </div>
+
+
+    <!-- /.navbar -->
+    @yield('menu')
+    <div class="content-wrapper">
+        <div class="container-fluid pt-3">
+            @yield('content')
+        </div>
+    </div>
+    <!-- /.content-wrapper -->
+    <footer class="main-footer">
+        <strong>Copyright &copy; {{date('Y')}} <a href="https://adminlte.io">Clips Web</a>.</strong>
+
+        <div class="float-right d-none d-sm-inline-block">
+            {{--<b>Version</b> 3.2.0-rc--}}
+        </div>
+    </footer>
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="{{asset('vendor/lte/plugins/jquery/jquery.min.js')}}"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="{{asset('vendor/lte/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+    $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('vendor/lte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- ChartJS -->
+<script src="{{asset('vendor/lte/plugins/chart.js/Chart.min.js')}}"></script>
+<!-- Sparkline -->
+<script src="{{asset('vendor/lte/plugins/sparklines/sparkline.js')}}"></script>
+<!-- JQVMap -->
+<script src="{{asset('vendor/lte/plugins/jqvmap/jquery.vmap.min.js')}}"></script>
+<script src="{{asset('vendor/lte/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
+<!-- jQuery Knob Chart -->
+<script src="{{asset('vendor/lte/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
+<!-- daterangepicker -->
+<script src="{{asset('vendor/lte/plugins/moment/moment.min.js')}}"></script>
+<script src="{{asset('vendor/lte/plugins/daterangepicker/daterangepicker.js')}}"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="{{asset('vendor/lte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+<!-- Summernote -->
+<script src="{{asset('vendor/lte/plugins/summernote/summernote-bs4.min.js')}}"></script>
+<!-- overlayScrollbars -->
+<script src="{{asset('vendor/lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('vendor/lte/dist/js/adminlte.js')}}"></script>
+<!-- AdminLTE for demo purposes -->
+{{--<script src="{{asset('lte/dist/js/demo.js')}}"></script>--}}
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{asset('vendor/lte/dist/js/pages/dashboard.js')}}"></script>
+<script src="{{asset('vendor/lte/plugins/sweetalert2/sweetalert2.min.js')}}"></script>
+<script src="{{asset('vendor/lte/plugins/toastr/toastr.min.js')}}"></script>
+<!--Via cep-->
+<script src="{{asset('js/viacep.js')}}"></script>
+
+<!--Mask-->
+<script src="{{asset('js/jquery.mask.js')}}"></script>
+<script src="{{asset('js/mask.js')}}"></script>
+
+@include('base::utils.alert')
+</body>
+</html>
