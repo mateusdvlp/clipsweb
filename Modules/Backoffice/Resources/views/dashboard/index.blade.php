208c886699c4b08a12286a05148af876664d037c
mateusgaudencio14 <mateusdvlp@gmail.com>
2022-03-24 15:19:35 -0300

    update index, create

--- Modules/Backoffice/Resources/views/dashboard/index.blade.php
+++ Modules/Backoffice/Resources/views/dashboard/index.blade.php
@@ -5,7 +5,7 @@
         <div class="col-md-7 col-12">
             <div class="card">
                 <div class="card-header border-transparent">
-                    <h3 class="card-title">Latest Orders</h3>
+                    <h3 class="card-title">Novas contas</h3>
                     <div class="card-tools">
                         <button type="button" class="btn btn-tool" data-card-widget="collapse">
                             <i class="fas fa-minus"></i>
@@ -28,69 +28,7 @@
                             </tr>
                             </thead>
                             <tbody>
-                            <tr>
-                                <td><a href="pages/examples/invoice.html">OR9842</a></td>
-                                <td>Call of Duty IV</td>
-                                <td><span class="badge badge-success">Shipped</span></td>
-                                <td>
-                                    <div class="sparkbar" data-color="#00a65a" data-height="20">90,80,90,-70,61,-83,63
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td><a href="pages/examples/invoice.html">OR1848</a></td>
-                                <td>Samsung Smart TV</td>
-                                <td><span class="badge badge-warning">Pending</span></td>
-                                <td>
-                                    <div class="sparkbar" data-color="#f39c12" data-height="20">90,80,-90,70,61,-83,68
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td><a href="pages/examples/invoice.html">OR7429</a></td>
-                                <td>iPhone 6 Plus</td>
-                                <td><span class="badge badge-danger">Delivered</span></td>
-                                <td>
-                                    <div class="sparkbar" data-color="#f56954" data-height="20">90,-80,90,70,-61,83,63
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td><a href="pages/examples/invoice.html">OR7429</a></td>
-                                <td>Samsung Smart TV</td>
-                                <td><span class="badge badge-info">Processing</span></td>
-                                <td>
-                                    <div class="sparkbar" data-color="#00c0ef" data-height="20">90,80,-90,70,-61,83,63
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td><a href="pages/examples/invoice.html">OR1848</a></td>
-                                <td>Samsung Smart TV</td>
-                                <td><span class="badge badge-warning">Pending</span></td>
-                                <td>
-                                    <div class="sparkbar" data-color="#f39c12" data-height="20">90,80,-90,70,61,-83,68
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td><a href="pages/examples/invoice.html">OR7429</a></td>
-                                <td>iPhone 6 Plus</td>
-                                <td><span class="badge badge-danger">Delivered</span></td>
-                                <td>
-                                    <div class="sparkbar" data-color="#f56954" data-height="20">90,-80,90,70,-61,83,63
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td><a href="pages/examples/invoice.html">OR9842</a></td>
-                                <td>Call of Duty IV</td>
-                                <td><span class="badge badge-success">Shipped</span></td>
-                                <td>
-                                    <div class="sparkbar" data-color="#00a65a" data-height="20">90,80,90,-70,61,-83,63
-                                    </div>
-                                </td>
-                            </tr>
+
                             </tbody>
                         </table>
                     </div>
@@ -107,7 +45,7 @@
         <div class="col-md-5 col-12">
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">Recently Added Products</h3>
+                    <h3 class="card-title">Novos anúncios</h3>
                     <div class="card-tools">
                         <button type="button" class="btn btn-tool" data-card-widget="collapse">
                             <i class="fas fa-minus"></i>
@@ -119,71 +57,30 @@
                 </div>
 
                 <div class="card-body p-0">
-                    <ul class="products-list product-list-in-card pl-2 pr-2">
-                        <li class="item">
-                            <div class="product-img">
-                                <img src="dist/img/default-150x150.png" alt="Product Image" class="img-size-50">
-                            </div>
-                            <div class="product-info">
-                                <a href="javascript:void(0)" class="product-title">Samsung TV
-                                    <span class="badge badge-warning float-right">$1800</span></a>
-                                <span class="product-description">
-Samsung 32" 1080p 60Hz LED Smart HDTV.
-</span>
-                            </div>
-                        </li>
+                    <div class="table-responsive">
+                        <table class="table m-0">
+                            <thead>
+                            <tr>
+                                <th>Order ID</th>
+                                <th>Item</th>
+                                <th>Status</th>
+                                <th>Popularity</th>
+                            </tr>
+                            </thead>
+                            <tbody>
 
-                        <li class="item">
-                            <div class="product-img">
-                                <img src="dist/img/default-150x150.png" alt="Product Image" class="img-size-50">
-                            </div>
-                            <div class="product-info">
-                                <a href="javascript:void(0)" class="product-title">Bicycle
-                                    <span class="badge badge-info float-right">$700</span></a>
-                                <span class="product-description">
-26" Mongoose Dolomite Men's 7-speed, Navy Blue.
-</span>
-                            </div>
-                        </li>
-
-                        <li class="item">
-                            <div class="product-img">
-                                <img src="dist/img/default-150x150.png" alt="Product Image" class="img-size-50">
-                            </div>
-                            <div class="product-info">
-                                <a href="javascript:void(0)" class="product-title">
-                                    Xbox One <span class="badge badge-danger float-right">
-$350
-</span>
-                                </a>
-                                <span class="product-description">
-Xbox One Console Bundle with Halo Master Chief Collection.
-</span>
-                            </div>
-                        </li>
-
-                        <li class="item">
-                            <div class="product-img">
-                                <img src="dist/img/default-150x150.png" alt="Product Image" class="img-size-50">
-                            </div>
-                            <div class="product-info">
-                                <a href="javascript:void(0)" class="product-title">PlayStation 4
-                                    <span class="badge badge-success float-right">$399</span></a>
-                                <span class="product-description">
-PlayStation 4 500GB Console (PS4)
-</span>
-                            </div>
-                        </li>
-
-                    </ul>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
 
                 <div class="card-footer text-center">
-                    <a href="javascript:void(0)" class="uppercase">View All Products</a>
+                    <a href="javascript:void(0)" class="uppercase">Visualizar todos</a>
                 </div>
 
             </div>
         </div>
     </div>
+
 
 @endsection
