64c4e067e403276217f121c82564f6563ca13fd9
mateusgaudencio14 <mateusdvlp@gmail.com>
2022-03-25 10:42:58 -0300

    update template master

--- Modules/Backoffice/Resources/views/layouts/master.blade.php
+++ Modules/Backoffice/Resources/views/layouts/master.blade.php
@@ -175,8 +175,6 @@
             <nav class="mt-2">
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                     data-accordion="false">
-                    <!-- Add icons to the links using the .nav-icon class
-                         with font-awesome or any other icon font library -->
                     <li class="nav-item">
                         <a href="{{route('backoffice.dashboard-index')}}" class="nav-link">
                             <i class="fa fa-tachometer-alt"></i>
